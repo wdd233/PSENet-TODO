ee37c72b0ffb55f5c56328c2fce84f54bf6d72f7
王煜凯 <1556905690@qq.com>
2019-01-27 15:11:01 +0800

    upload files

--- model/fpn.py
+++ model/fpn.py
@@ -2,15 +2,51 @@ import torch
 import math
 import torch.nn as nn
 
+
+
+class ConvBasic(nn.Module):
+    def __init__(self, in_channels=1, out_channels=1, k=7, same=True):
+        super(ConvBasic, self).__init__()
+
+        self.conv = nn.Conv2d(in_channels, out_channels, k, padding= (k - 1) // 2 if same else 0)
+        self.bn = nn.BatchNorm2d(out_channels)
+        self.activation = nn.ReLU(inplace=True)
+    def forward(self, x):
+        return self.activation(self.bn(self.conv(x)))
+
+class BottleNeck(nn.Module):
+    expansion = 4
+    def __init__(self, in_planes, planes, stride=1, downsample=None):
+        super(BottleNeck, self).__init__()
+        self.conv1 = nn.Conv2d(in_planes, planes, kernel_size=1, bias=False)
+        self.bn1 = nn.BatchNorm2d(planes)
+        self.conv2 = nn.Conv2d(planes, planes, kernel_size=3, stride=stride, padding=1, bias=False)
+        self.bn2 = nn.BatchNorm2d(planes)
+        self.conv3 = nn.Conv2d(planes, self.expansion * planes, kernel_size=1, bias= False)
+        self.bn3 = nn.BatchNorm2d(self.expansion * planes)
+        self.relu = nn.ReLU(inplace=True)
+        self.downsample = downsample
+    def forward(self, x):
+        residual = x
+        out = self.conv1(x)
+        out = self.bn1(out)
+
+        out = self.conv2(out)
+        out = self.bn2(out)
+
+        out = self.conv3(out)
+        out = self.bn3(out)
+        if self.downsample is not None:
+            residual = self.downsample(x)
+        return self.relu(residual + out)
+
 class FPN(nn.Module):
-    def __init__(self, block, layers, in_channels):
+    def __init__(self, block, layers, in_channels=3, upsample_mode = True):
         super(FPN, self).__init__()
         self.inplanes = 64
         self.in_channels = in_channels
-        self.conv1 = nn.Conv2d(3, 64, kernel_size=7, stride=2, padding=3)
-        self.bn1 = nn.BatchNorm2d(64)
-        self.relu = nn.ReLU(inplace=True)
-        self.maxpool = nn.MaxPool2d(kernel_size=3, stride=2, padidng=1)
+        self.conv1 = ConvBasic(self.in_channels, 64, 7)
+        self.maxpool = nn.MaxPool2d(kernel_size=3, stride=2, padding=1)
         self.layer1 = self._make_layers(block, 64, layers[0])
         self.layer2 = self._make_layers(block, 128, layers[1])
         self.layer3 = self._make_layers(block, 256, layers[2])
@@ -25,16 +61,21 @@ class FPN(nn.Module):
         self.latlayer1 = nn.Conv2d(1024, 256, kernel_size=1, stride=1, padding=0)
         self.latlayer2 = nn.Conv2d(512, 256, kernel_size=1, stride=1, padding=0)
         self.latlayer3 = nn.Conv2d(256, 256, kernel_size=1, stride=1, padding=0)
+
+        self.upLayer = nn.ConvTranspose2d(256, 256, kernel_size=1) if not upsample_mode else nn.Upsample(scale_factor=2, mode='bilinear', align_corners=True)
+        self.upLayer2 = nn.ConvTranspose2d(256, 256, kernel_size=1) if not upsample_mode else nn.Upsample(scale_factor=2, mode='bilinear')
+        self.upLayer3 = nn.ConvTranspose2d(256, 256, kernel_size=1) if not upsample_mode else nn.Upsample(scale_factor=2, mode='bilinear', align_corners=True)
+
         self._weight_init()
 
     def _weight_init(self):
         for m in self.modules():
-            if isinstance(m, nn.Conv2d):
-                n = m.kernel_size[9] * m.kernel_size[1] * m.out_channels
+            if isinstance(m, nn.Conv2d) or isinstance(m, nn.ConvTranspose2d):
+                n = m.kernel_size[0] * m.kernel_size[1] * m.out_channels
                 m.weight.data.normal_(0, math.sqrt(2./n))
             elif isinstance(m, nn.BatchNorm2d):
                 m.weight.data.fill_(1)
-                m.bias.dta.zero_()
+                m.bias.data.zero_()
     def _make_layers(self, block, planes, blocks, stride=1):
         downsample = None
         if stride != 1 or self.inplanes !=block.expansion * planes:
@@ -48,6 +89,35 @@ class FPN(nn.Module):
             layers.append(block(self.inplanes, planes))
         return nn.Sequential(*layers)
 
-    def upsample_add(self, mode=''):
+    # def upsample_add(self, x, y, mode='bilinear'):
+    #     _, _, H, W = y.size()
+    #     if mode == 'bilinear':
+    #         self.up = nn.Upsample(scale_factor=2, mode='bilinear', align_corners=True)
+    #     else:
+    #         self.up = nn.ConvTranspose2d()
+    #     return F.upsample(x, ) if mode == 'bilinear' else nn.ConvTranspose2d(s)
+
+    def forward(self, x):
+        x = self.conv1(x)
+        c1 = self.maxpool(x)
+        c2 = self.layer1(c1)
+        c3 = self.layer2(x)
+        c4 = self.layer3(x)
+        c5 = self.layer4(x)
 
 
+        p5 = self.toplayer(c5)
+        p4 = self.upLayer1(p5) + self.latlayer1(c4)
+        p3 = self.upLayer2(p4) + self.latlayer2(c3)
+        p2 = self.upLayer3(p3) + self.latlayer2(c2)
+        return [p2, p3, p4, p5]
+
+
+if __name__ == '__main__':
+    a = torch.randn(1, 3, 30, 30)
+    model = FPN(BottleNeck, [3, 4, 6, 3])
+    # for i in model.named_modules():
+    #     print(i)
+    # print(model)
+    b = model(a)
+    print(b)
--- pse.py
+++ pse.py
@@ -1,12 +1,11 @@
 import numpy as np
-import cv2
 from collections import deque
 import skimage.measure
 def pse(kernals, min_area):
     kernal_num = len(kernals)#判断里面有几个kernel
     pred = np.zeros(kernals[0].shape, dtype='int32')#最终的预测输出
     
-    label, label_num = skimage.measure.label(kernals[kernal_num - 1], neighbors=4, return_num=True)#四邻域标签，返回
+    label, label_num = skimage.measure.label(kernals[0], neighbors=4, return_num=True)#四邻域标签，返回
     
     for label_idx in range(1, label_num):#小范围区域看成噪声,把面积小于阈值像素的位置置为空
         if np.sum(label == label_idx) < min_area:
@@ -14,7 +13,7 @@ def pse(kernals, min_area):
 
     queue = deque()
     next_queue = deque()
-    points = np.array(np.where(label > 0)).transpose((1, 0))#points存储所有点坐标(x,y)
+    points = np.array(np.where(label > 0)).transpose((1, 0))#初始化points存储所有点坐标(x,y)
     
     for point_idx in range(points.shape[0]):#遍历每一个点
         x, y = points[point_idx, 0], points[point_idx, 1]#取出每一个点的坐标
@@ -25,18 +24,17 @@ def pse(kernals, min_area):
 
     dx = [-1, 1, 0, 0]
     dy = [0, 0, -1, 1]
-    for kernal_idx in range(kernal_num - 2, -1, -1):#遍历每一个kernel
+    for kernal_idx in range(1, kernal_num ):#遍历每一个kernel
         kernal = kernals[kernal_idx].copy()
         while len(queue):
             (x, y, l) = queue.popleft()#点出队列
-
             is_edge = True
             for j in range(4):#邻域判断
                 tmpx = x + dx[j]#四个邻域遍历
                 tmpy = y + dy[j]
                 if tmpx < 0 or tmpx >= kernal.shape[0] or tmpy < 0 or tmpy >= kernal.shape[1]:
                     continue
-                if kernal[tmpx, tmpy] == 0 or pred[tmpx, tmpy] > 0:
+                if kernal[tmpx, tmpy] == 0 or pred[tmpx, tmpy] > 0:#p>0说明该点已经用过了
                     continue
                 queue.append((tmpx, tmpy, l))
                 # queue.put((tmpx, tmpy, l))
@@ -67,7 +65,7 @@ if __name__ == '__main__':
     s2[[2, 2, 2, 3, 3, 3], [0, 1, 2, 0, 1, 2]] = 1
     s3[[1,1,1,1],[0,1,2,3]] = 1
     # com = np.concatenate((x,y,x,y),axis=2)
-    kernels = np.stack((s1,s2, s3))
+    kernels = np.stack((s1,s3, s2))
     print(kernels)
     pred = pse(kernels, 2)
     print(pred)
